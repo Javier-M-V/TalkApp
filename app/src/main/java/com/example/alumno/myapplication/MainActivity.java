311fd0c65868868882bea434080b3d4e8d9e91a9
Javier <javier.moreno.vilaplana@gmail.com>
2017-09-25 12:29:53 +0200

    Inicio

--- app/src/main/java/com/example/alumno/myapplication/MainActivity.java
+++ app/src/main/java/com/example/alumno/myapplication/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.inicio);//R es rscuros.layout.el activity
+        setContentView(R.layout.inicio);
     }
 }
