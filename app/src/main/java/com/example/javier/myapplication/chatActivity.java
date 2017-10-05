3f22dd967d35849fe9d0693df5b2798b65edccc9
Javier <javier.moreno.vilaplana@gmail.com>
2017-10-05 08:55:27 +0200

    new mensaje class and class chatActivity

--- app/src/main/java/com/example/javier/myapplication/chatActivity.java
+++ app/src/main/java/com/example/javier/myapplication/chatActivity.java
@@ -2,21 +2,23 @@ package com.example.javier.myapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class chatActivity extends Activity {
 
+    String[] charla={"HOLA","KE","ASE","LOKO"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.chat);
-
-
-        TextView tx = null;
-        String mensajes[] = {"Hola", "cosa feliz", "bocadillo", "de pan"};
-        for (int i = 0; i < mensajes.length; i++) {
-            tx = new TextView(this);
-            tx.setText(mensajes[i]);
+        setContentView(R.layout.chat);;
+        LinearLayout lay_smg =findViewById(R.id.Lay_mensajes);
+        TextView txtv = null;
+        for(int i = 0; i<charla.length;i++){
+            txtv= new TextView(this);
+            txtv.setText(charla[i]);
+            lay_smg.addView(txtv);
         }
     }
+
 }
